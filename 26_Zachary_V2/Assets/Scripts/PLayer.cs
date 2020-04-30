7c9b240760021db05e9ec67df6b58430c4142414
NezuNyan <64115041+NezuNyan@users.noreply.github.com>
2020-04-30 12:43:46 +0800

    Created unity project

---
+++ 26_Zachary_V2/Assets/Scripts/PLayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayer : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float speed;
+    public float rotationspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertmove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float hormove = Input.GetAxis("Horizontal") * rotationspeed *Time.deltaTime;
+
+        transform.Translate(hormove, 0, vertmove);
+
+    }
+}
