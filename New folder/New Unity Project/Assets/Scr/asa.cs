36a61d91a9f901ab9fc20c613ef565dc11be7894
NezuNyan <64115041+NezuNyan@users.noreply.github.com>
2020-04-30 12:57:35 +0800

    created version 2 unity

---
+++ New folder/New Unity Project/Assets/Scr/asa.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asa : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forward = Input.GetAxis("Vertical") * 3 * Time.deltaTime;
+        float side = Input.GetAxis("Horizontal") * 3 * Time.deltaTime;
+
+        transform.Translate(side, 0, forward);
+
+    }
+}
